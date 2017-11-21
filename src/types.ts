1ad3c3593caa360390684d4b730c6e84a7e25560
Daniel Imfeld <daniel@danielimfeld.com>
2017-11-20 19:35:05 -1000

    Add function for formatting expiration. Make symbol padding optional

--- src/types.ts
+++ src/types.ts
@@ -10,11 +10,18 @@ export interface OptionLeg {
   price? : number;
 }
 
-export function fullSymbol(ol: OptionLeg) {
+export function occExpirationFromDate(d : Date) {
+  let year = d.getUTCFullYear().toString().slice(2);
+  let month = _.padStart((d.getUTCMonth() + 1).toString(), 2, '0');
+  let day = _.padStart(d.getUTCDate().toString(), 2, '0');
+  return `${year}${month}${day}`;
+}
+
+export function fullSymbol(ol: OptionLeg, padSymbol=true) {
   if(!_.isNil(ol.call) && ol.strike) {
     let legType = ol.call ? 'C' : 'P';
     let strike = _.padStart((ol.strike * 1000).toString(), 8, '0').slice(0, 8);
-    let symbol = _.padEnd(ol.symbol, 6, ' ');
+    let symbol = padSymbol ? _.padEnd(ol.symbol, 6, ' ') : ol.symbol;
     return `${symbol}${ol.expiration}${legType}${strike}`;
   } else {
     // Otherwise it's just an equity.
